8d2075a7e4be773b2fa37b9f48a0254b43c85bc0
Andrey Alferov <alferov@andytower.ru>
2016-12-16 15:12:30 +0300

    Создание упрощенной версии. Удаление прослушивания клавиатуры

--- MainProgram.py
+++ MainProgram.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'tsa'
+__author__ = 'andytower'
+
 from ConfigManager import ConfigManager
 from GuiManager import GuiManager
 from KeyboardStatus import keyboardStatus
+from tkinter import Tk
 
 class ThreadedClient:
     def __init__(self, master):
         self.master = master
+
+        master.attributes('-alpha', 0.5)
 
         self.config=ConfigManager()
         self.keyTrainer=keyboardStatus(self.config)
@@ -18,13 +23,16 @@ class ThreadedClient:
 
         self.guiManager=GuiManager(master,self.config,keyTrainer.myQueue,keyTrainer)
 
-        keyTrainer.begin_scan()
+        #keyTrainer.begin_scan()
 
         self.running = 1
         self.periodicCall()
+        master.mainloop()
+
     def kill_and_destroy(self):
+
         self.running = 0
-        self.keyTrainer.stop_scan()
+        #self.keyTrainer.stop_scan()
         if self.config.debug:
             print("Stopping scan...")
         self.master.destroy()
@@ -34,18 +42,12 @@ class ThreadedClient:
         self.guiManager.processQueue()
         if not self.running:
             # import sys
-            # sys.exit(1)
             self.kill_and_destroy()
         self.master.after(20, self.periodicCall)
 
 if __name__ == '__main__':
-    try:
-        import tkinter
-        root = tkinter.Tk()
+    root = Tk()
 
-        root.attributes("-alpha", 0.5)
-        client = ThreadedClient(root)
-        root.mainloop()
-    except ImportError:
-        print("Please install tkinter for python, on Ubuntu, Mint do following:\n"
-              "sudo apt-get install python-tk")
+
+    app = ThreadedClient(root)
+    #root.mainloop()

4d1d9559d707f1b8cfde0fee957fb4a885abb74a
Andrey Alferov <alferov@andytower.ru>
2016-12-16 15:25:04 +0300

    Удаление лишних комментариев и причесывание кода

--- MainProgram.py
+++ MainProgram.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 __author__ = 'tsa'
@@ -9,21 +9,22 @@ from GuiManager import GuiManager
 from KeyboardStatus import keyboardStatus
 from tkinter import Tk
 
+
 class ThreadedClient:
+
     def __init__(self, master):
         self.master = master
 
         master.attributes('-alpha', 0.5)
 
-        self.config=ConfigManager()
-        self.keyTrainer=keyboardStatus(self.config)
-        keyTrainer=self.keyTrainer
+        self.config = ConfigManager()
+        self.keyTrainer = keyboardStatus(self.config)
+        keyTrainer = self.keyTrainer
 
         master.protocol('WM_DELETE_WINDOW', self.kill_and_destroy)
 
-        self.guiManager=GuiManager(master,self.config,keyTrainer.myQueue,keyTrainer)
-
-        #keyTrainer.begin_scan()
+        self.guiManager = GuiManager(master, self.config,
+                keyTrainer.myQueue, keyTrainer)
 
         self.running = 1
         self.periodicCall()
@@ -32,22 +33,16 @@ class ThreadedClient:
     def kill_and_destroy(self):
 
         self.running = 0
-        #self.keyTrainer.stop_scan()
-        if self.config.debug:
-            print("Stopping scan...")
         self.master.destroy()
-
 
     def periodicCall(self):
         self.guiManager.processQueue()
         if not self.running:
-            # import sys
             self.kill_and_destroy()
         self.master.after(20, self.periodicCall)
+
 
 if __name__ == '__main__':
     root = Tk()
 
-
     app = ThreadedClient(root)
-    #root.mainloop()
